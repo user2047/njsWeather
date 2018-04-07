b54d7128012c19a1deb1b5c9ce7d777f8c7149e1
cam sen <cameron.senna@gmail.com>
2018-04-06 22:25:25 -0400

    mobile commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const request = require("request");
 const yargs = require("yargs");
 
-const argv = yargs
+const argv = yargs.options({
+	a: {
+	demand: true,
+	alias: "address",
+	describe: "Address to fetch weather for",
+	string: true
+	}
+})
+.help()
+.alias("help","h")
+.argv;
+
+var address = argv.a;
+var encodedAddress = encodeURIComponent(address);
 
 request({
-  url: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyDtV1OIoPautzYVaGH1WAYPYKeAcu_QKbo",
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDtV1OIoPautzYVaGH1WAYPYKeAcu_QKbo`,
   json: true
 },(error,response,body) =>{
 // console.log(JSON.stringify(body, undefined, 2));
